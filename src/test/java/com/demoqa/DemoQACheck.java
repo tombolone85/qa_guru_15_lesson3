351b334d00537f3a9519a88c21a668be42b1762b
Василий Мараховский <tombolone@yandex.ru>
2022-09-28 19:45:52 +0400

    lesson 6 homework - all values to be inputted and checked is transferred to variables

--- src/test/java/com/demoqa/DemoQACheck.java
+++ src/test/java/com/demoqa/DemoQACheck.java
@@ -17,6 +17,19 @@ public class DemoQACheck {
     String email = faker.internet().emailAddress();
     String address = faker.address().fullAddress();
     String file = faker.internet().image();
+    String gender = "Male";
+    String day = "28";
+    String month = "July";
+    String year = "1985";
+    String subject = "Physics";
+    String hobby = "Reading";
+    String filePath = "src/test/resources/5.png";
+    String state = "Haryana";
+    String city = "Panipat";
+    String fileName = "5.png";
+    String birthday = "28 July,1985";
+    String stateAndCity = "Haryana Panipat";
+
     @BeforeAll
     public static void сonfiguration() {
         Configuration.browserSize="1920x1080";
@@ -29,15 +42,15 @@ public class DemoQACheck {
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .setEmail(email)
-                .genderClick("Male")
+                .genderClick(gender)
                 .setMobile(mobile)
-                .setBirthDate("28","July","1985")
-                .setSubjects("Physics")
-                .setHobbies("Reading")
-                .uploadPicture("src/test/resources/5.png")
+                .setBirthDate(day,month,year)
+                .setSubjects(subject)
+                .setHobbies(hobby)
+                .uploadPicture(filePath)
                 .setAddress(address)
-                .setState("Haryana")
-                .setCity("Panipat")
+                .setState(state)
+                .setCity(city)
                 .clickSubmit();
     }
     @Test
@@ -45,13 +58,13 @@ public class DemoQACheck {
         registrationFormPage.modalTableIsVisible()
             .checkTableContent("Student Name",studentName)
         .checkTableContent("Student Email",email)
-        .checkTableContent("Gender","Male")
+        .checkTableContent("Gender", gender)
         .checkTableContent("Mobile",mobile)
-        .checkTableContent("Date of Birth","28 July,1985")
-        .checkTableContent("Subjects","Physics")
-        .checkTableContent("Hobbies","Reading")
-        .checkTableContent("Picture","5.png")
+        .checkTableContent("Date of Birth",birthday)
+        .checkTableContent("Subjects",subject)
+        .checkTableContent("Hobbies",hobby)
+        .checkTableContent("Picture",fileName)
         .checkTableContent("Address",address)
-        .checkTableContent("State and City","Haryana Panipat");
+        .checkTableContent("State and City",stateAndCity);
     }
 }
