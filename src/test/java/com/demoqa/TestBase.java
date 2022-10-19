ec4f23f6598ab8efdf8f08805b94e78590f78cc3
Василий Мараховский <tombolone@yandex.ru>
2022-10-19 18:30:26 +0300

    lesson 6 homework - all values to be inputted and checked is transferred to variables - split to different classes - allure-results - corrected TestBase - resolution increased - allure-results renamed

--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -2,7 +2,9 @@ package com.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -13,4 +15,11 @@ public class TestBase {
         Configuration.holdBrowserOpen=true;
         Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+    }
+
 }
