a05fdf254377353be082acd490dbd6a4bafb5146
Василий Мараховский <tombolone@yandex.ru>
2022-10-19 04:12:01 +0300

    lesson 6 homework - all values to be inputted and checked is transferred to variables - split to different classes - allure-results - corrected TestBase - resolution increased

--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
     @BeforeAll
     public static void configuration() {
-        Configuration.browserSize="1920x1080";
+        Configuration.browserSize="5760x3240";
         Configuration.holdBrowserOpen=true;
         Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
