e7f0c3d2166b8a0d554cd90615b249a513e6dbdf
Василий Мараховский <tombolone@yandex.ru>
2022-10-20 15:22:47 +0300

    lesson 6 homework - all values to be inputted and checked is transferred to variables - split to different classes - allure-results - corrected TestBase - resolution increased - allure-results renamed

--- src/test/java/com/demoqa/TestBase.java
+++ src/test/java/com/demoqa/TestBase.java
@@ -6,10 +6,26 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URI;
+
+import static io.qameta.allure.internal.shadowed.jackson.databind.type.LogicalType.Map;
+import static java.lang.constant.ClassDesc.of;
 
 public class TestBase {
     @BeforeAll
     public static void configuration() {
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName","chrome");
+//        capabilities.setCapability("browserVersion","100.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo",true);
+        
+        Configuration.browserCapabilities =capabilities;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize="5760x3240";
         Configuration.holdBrowserOpen=true;
